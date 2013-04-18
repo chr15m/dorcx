69b51f5e8af3bc93611f40e0f3a338db8628664f
Chris McCormick <chris@mccormick.cx>
2013-04-18 16:00:47 +0800

    Empty front page showing up. Added basic imap code for setting up folders.

---
+++ server/dorcx/imapdb.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import imaplib
+import re
+import socket
+
+total_re = re.compile("MESSAGES (?P<total>\d+)")
+unread_re = re.compile("UNSEEN (?P<unread>\d+)")
+
+# TODO: persist multiple imap connections in some sensible way
+class DorcxImapDb:
+	def __init__(self, email, password, username=None, domain=None):
+		# TODO: validate email first
+		emailparts = email.split("@")
+		# get the username from the email address
+		if username is None:
+			username = emailparts[0]
+		# get the domain name from the email address
+		if domain is None:
+			domain = emailparts[1]
+		# TODO: test various ways of logging in (Non-SSL), common server subdomains, etc.
+		# TODO: catch e.g. socket.gaierror: [Errno -2] Name or service not known
+		self.m = imaplib.IMAP4_SSL(domain)
+		# TODO: catch e.g. imaplib.error: [AUTHENTICATIONFAILED] Authentication failed.
+		self.m.login(username, password)
+	
+	def setup_dorcx_folders(self):
+		""" Sets up the dorcx subfolder and it's subfolders - config, private, public. """
+		# TODO: catch errors properly
+		# TODO: ignore this error though:
+		# ('NO', ['[ALREADYEXISTS] Mailbox exists.'])
+		result = self.m.create("dorcx/")
+		if result and len(result) and result[0] == 'NO':
+			return {"error": result}
+		else:
+			self.m.create("dorcx/config")
+			self.m.create("dorcx/inbox")
+			self.m.create("dorcx/public/")
+			self.m.create("dorcx/public/config")
+			self.m.create("dorcx/public/outbox")
+			self.m.create("dorcx/private/")
+			self.m.create("dorcx/private/config")
+			self.m.create("dorcx/private/outbox")
+			return True
+	
+	def get_unread_count(self, boxes=[]):
+		""" Returns unread count for the user's actual INBOX folder. """
+		try:
+			for b in ['INBOX'] + boxes:
+				r = self.m.status(b, '(UNSEEN MESSAGES)')[1][0]
+				total = total_re.search(r).groupdict()["total"]
+				unread = unread_re.search(r).groupdict()["unread"]
+				return [unread, total]
+		except socket.gaierror, e:
+			return {"error": e}
+
--- server/dorcx/urls.py
+++ server/dorcx/urls.py
@@ -1,17 +1,29 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
+import settings
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'dorcx.views.home', name='home'),
-    # url(r'^dorcx/', include('dorcx.foo.urls')),
+	url(r'^$', direct_to_template, {"template": "client/index.html"}, name='home'),
+	
+	# Examples:
+	# url(r'^$', 'dorcx.views.home', name='home'),
+	# url(r'^dorcx/', include('dorcx.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	# Uncomment the admin/doc line below to enable admin documentation:
+	# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+	# Uncomment the next line to enable the admin:
+	# url(r'^admin/', include(admin.site.urls)),
 )
+
+# statically serve this stuff while developing
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
